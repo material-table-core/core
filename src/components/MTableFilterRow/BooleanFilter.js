547fbb682b75cb095e398030b0c669423733b013
Vlad Mihalachi <vmihalachi@outlook.com>
2021-01-13 09:36:45 +0200

    Update BooleanFilter.js

--- src/components/MTableFilterRow/BooleanFilter.js
+++ src/components/MTableFilterRow/BooleanFilter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormControlLabel, Checkbox } from '@material-ui/core';
+import { Checkbox } from '@material-ui/core';
 
 export default function BooleanFilter({ columnDef, onFilterChanged }) {
   return (
