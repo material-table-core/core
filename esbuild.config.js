462dbe16636a9eacf812b2db93b234a25362f542
Matt Oestreich <matthewpoestreich@gmail.com>
2021-01-14 21:05:59 -0600

    Update esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,23 +1,17 @@
 const { build } = require('esbuild');
-const { red, green, yellow, italic, greenBright } = require('chalk');
+const { red, green, yellow, italic } = require('chalk');
 const rimraf = require('rimraf');
 const path = require('path');
-
-const pkg = require('./package.json');
+const fs = require('fs');
 
 const { log } = console;
 const { stdout, stderr, exit } = process;
 
-const peerDeps = Object.keys(pkg.peerDependencies).map((k) => k);
-
-// Options
 const BUILD_DIR = 'dist'; // relative to root of project (no trailing slash)
-const BUNDLE_FILE_NAME = 'material-table.js'; // bundle file name (no starting slash)
-const ENTRY_POINT = 'src/index.js'; // relative to root of project
 
 stdout.write(yellow(`-Cleaning build artifacts from : '${BUILD_DIR}' `));
 
-rimraf(path.resolve(__dirname, BUILD_DIR), (error) => {
+rimraf(path.resolve(__dirname, BUILD_DIR), async (error) => {
   if (error) {
     stderr.write(red(`err cleaning '${BUILD_DIR}' : ${error.stderr}`));
     exit(1);
@@ -26,49 +20,52 @@ rimraf(path.resolve(__dirname, BUILD_DIR), (error) => {
   log(green.italic('successfully cleaned build artifacts'));
 
   const options = {
-    entryPoints: [ENTRY_POINT],
+    entryPoints: getFilesRecursive('./src', '.js'),
     minify: true,
-    bundle: true,
-    format: 'esm',
-    outfile: `${BUILD_DIR}/${BUNDLE_FILE_NAME}`,
+    // format: 'esm',
+    outdir: `${BUILD_DIR}`,
     loader: {
       '.js': 'jsx'
-    },
-    // These are third party libraries that we DO NOT want to
-    // include in our bundle. It is assumed the consumer will
-    // already have these packages. Including them in our bundle
-    // only bloats the size for no reason.
-    external: [
-      ...peerDeps,
-      'react-beautiful-dnd',
-      /material-ui/,
-      'jspdf',
-      'jspdf-autotable',
-      '@material-ui',
-      'react-is',
-      'classnames',
-      'object-assign',
-      'prop-types'
-    ]
+    }
   };
 
   log(yellow('-Begin bundling'));
-  build(options)
-    .then(() => {
-      // Only using `.then` for logging purposes
-      log(
-        greenBright(
-          `\nSuccessfully bundled to ${BUILD_DIR}/${BUNDLE_FILE_NAME}`
-        ),
-        yellow('\n[note]'),
-        italic.green(': this path is relative to the root of this project)')
-      );
-    })
-    .catch((error) => {
-      stderr.write(red(`\nerror bundling : ${error.stderr}`));
-      exit(1);
-    })
-    .finally(() => {
-      exit(0);
-    });
+
+  try {
+    await build(options);
+    log(
+      green(`\nSuccessfully bundled to '${BUILD_DIR}'`),
+      yellow('\n[note]'),
+      italic.green(': this path is relative to the root of this project)')
+    );
+  } catch {
+    stderr.write(red(`\nerror bundling : ${error.stderr}`));
+    exit(1);
+  } finally {
+    exit(0);
+  }
 });
+
+/**
+ * Helper functions
+ */
+
+function getFilesRecursive(dirPath, fileExtension = '') {
+  const paths = traverseDir(dirPath);
+  if (fileExtension === '') {
+    return paths;
+  }
+  return paths.filter((p) => p.endsWith(fileExtension));
+}
+
+function traverseDir(dir, filePaths = []) {
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = path.join(dir, file);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      traverseDir(fullPath, filePaths);
+    } else {
+      filePaths.push(fullPath);
+    }
+  });
+  return filePaths;
+}
