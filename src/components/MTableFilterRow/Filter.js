b485d0102df74c5622da4b4c5a729b24b6a04aaf
Domino987 <domino051@gmail.com>
2021-01-22 20:13:37 +0100

    Add forwardRef to Filter

--- src/components/MTableFilterRow/Filter.js
+++ src/components/MTableFilterRow/Filter.js
@@ -1,8 +1,13 @@
-import { createElement } from 'react';
+import React, { createElement } from 'react';
 
-export default function Filter({ columnDef, onFilterChanged }) {
+function Filter({ columnDef, onFilterChanged, forwardedRef }) {
   return createElement(columnDef.filterComponent, {
     columnDef,
     onFilterChanged,
+    forwardedRef
   });
 }
+
+export default React.forwardRef(function FilterRef(props, ref) {
+  return <Filter {...props} forwardedRef={ref} />;
+});
