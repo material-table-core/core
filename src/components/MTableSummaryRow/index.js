87b0e69afa6f784afbd8c070173c9085a3cc7644
Peace Chen <github@ghpc.33mail.com>
2021-04-07 18:06:39 -0500

    Account for action icons in summary row

--- src/components/MTableSummaryRow/index.js
+++ src/components/MTableSummaryRow/index.js
@@ -2,19 +2,75 @@ import * as React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { TableRow, TableCell } from '@material-ui/core';
 import { getStyle } from '../MTableCell/utils';
+import * as CommonValues from '../../utils/common-values';
 import PropTypes from 'prop-types';
 
 export function MTableSummaryRow({
   data,
   columns,
   currentData,
+  rowProps,
   renderSummaryRow
 }) {
   if (!renderSummaryRow) {
     return null;
   }
+
+  function renderPlaceholderColumn(key, numIcons = 1) {
+    const size = CommonValues.elementSize(rowProps);
+    const width = numIcons * CommonValues.baseIconSize(rowProps);
+    return (
+      <TableCell
+        key={`placeholder.${key}`}
+        size={size}
+        padding="none"
+        style={{
+          width: width,
+          padding: '0px 5px',
+          boxSizing: 'border-box'
+        }}
+      />
+    );
+  }
+  const placeholderLeftColumns = [];
+  const placeholderRightColumns = [];
+  let placeholderKey = 0;
+
+  // Create empty columns corresponding to selection, actions, detail panel, and tree data icons
+  if (rowProps.options.selection) {
+    placeholderLeftColumns.push(renderPlaceholderColumn(placeholderKey++));
+  }
+  if (
+    rowProps.actions &&
+    rowProps.actions.filter(
+      (a) => a.position === 'row' || typeof a === 'function'
+    ).length > 0
+  ) {
+    const numRowActions = CommonValues.rowActions(rowProps).length;
+    if (rowProps.options.actionsColumnIndex === -1) {
+      placeholderRightColumns.push(
+        renderPlaceholderColumn(placeholderKey++, numRowActions)
+      );
+    } else if (rowProps.options.actionsColumnIndex >= 0) {
+      placeholderLeftColumns.push(
+        renderPlaceholderColumn(placeholderKey++, numRowActions)
+      );
+    }
+  }
+  if (rowProps.detailPanel && rowProps.options.showDetailPanelIcon) {
+    if (rowProps.options.detailPanelColumnAlignment === 'right') {
+      placeholderRightColumns.push(renderPlaceholderColumn(placeholderKey++));
+    } else {
+      placeholderLeftColumns.push(renderPlaceholderColumn(placeholderKey++));
+    }
+  }
+  if (rowProps.isTreeData) {
+    placeholderLeftColumns.push(renderPlaceholderColumn(placeholderKey++));
+  }
+
   return (
     <TableRow>
+      {placeholderLeftColumns}
       {columns.map((column, index) => {
         const summaryColumn = renderSummaryRow({
           index,
@@ -45,6 +101,7 @@ export function MTableSummaryRow({
           </TableCell>
         );
       })}
+      {placeholderRightColumns}
     </TableRow>
   );
 }
