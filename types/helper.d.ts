adf479c3009a93969b869eeb77af2b193ab02f63
Domino987 <domino051@gmail.com>
2021-05-02 14:10:42 +0200

    Create helper.d.ts

---
+++ types/helper.d.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+
+type Handlers = Concrete<
+  Omit<
+    Omit<React.DOMAttributes<HTMLDivElement>, 'children'>,
+    'dangerouslySetInnerHTML'
+  >
+>;
+
+type OnHandlers<Type> = Partial<
+  {
+    [Property in keyof Handlers]: (
+      event: Parameters<Handlers[Property]>[0],
+      rowData: Type
+    ) => void;
+  }
+>;
+
+export type { OnHandlers };
