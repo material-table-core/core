255cc99123ad263d64c875ea8596db5d891b9c1e
Matt Oestreich <matthewpoestreich@gmail.com>
2021-06-08 22:46:45 -0500

    Update index.js

--- src/store/actions/table/index.js
+++ src/store/actions/table/index.js
@@ -1,13 +1,3 @@
-/**
- * For demo purposes
- */
-
-// export const counterActions = {
-//   SET_VAL: "SET_VAL",
-//   INCREMENT: "INCREMENT",
-//   DECREMENT: "DECREMENT",
-// }
-
 const tableActions = {
   SET_DATA: 'SET_DATA'
 };
