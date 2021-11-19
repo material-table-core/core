e52506626319ae3b789d29e378970954dffc09ec
kouraf <farouk.yeddou@gmail.com>
2021-11-19 23:32:56 +0100

    added default icons

--- src/defaults/props.icons.js
+++ src/defaults/props.icons.js
@@ -5,105 +5,51 @@
 /* eslint-disable react/display-name */
 import React, { forwardRef } from 'react';
 import { Icon } from '@material-ui/core';
+import {
+  AddBox,
+  ArrowDownward,
+  Check,
+  ChevronLeft,
+  ChevronRight,
+  Clear,
+  DeleteOutline,
+  Edit,
+  FilterList,
+  FirstPage,
+  LastPage,
+  Remove,
+  SaveAlt,
+  Search,
+  ViewColumn,
+  Replay
+} from '@material-ui/icons';
 
 export default {
-  Add: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      add_box
-    </Icon>
-  )),
-  Check: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      check
-    </Icon>
-  )),
-  Clear: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      clear
-    </Icon>
-  )),
-  Delete: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      delete_outline
-    </Icon>
-  )),
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
   DetailPanel: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      chevron_right
-    </Icon>
+    <ChevronRight {...props} ref={ref} />
   )),
-  Edit: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      edit
-    </Icon>
-  )),
-  Export: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      save_alt
-    </Icon>
-  )),
-  Filter: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      filter_list
-    </Icon>
-  )),
-  FirstPage: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      first_page
-    </Icon>
-  )),
-  LastPage: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      last_page
-    </Icon>
-  )),
-  NextPage: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      chevron_right
-    </Icon>
-  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
   PreviousPage: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      chevron_left
-    </Icon>
+    <ChevronLeft {...props} ref={ref} />
   )),
-  ResetSearch: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      clear
-    </Icon>
-  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
   Resize: forwardRef((props, ref) => (
-    <Icon
-      {...props}
-      ref={ref}
-      style={{ ...props.style, transform: 'rotate(90deg)' }}
-    >
-      drag_handle
+    <Icon {...props} ref={ref} style={{ ...props.style }}>
+      |
     </Icon>
   )),
-  Search: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      search
-    </Icon>
-  )),
-  SortArrow: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      arrow_downward
-    </Icon>
-  )),
-  ThirdStateCheck: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      remove
-    </Icon>
-  )),
-  ViewColumn: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      view_column
-    </Icon>
-  )),
-  Retry: forwardRef((props, ref) => (
-    <Icon {...props} ref={ref}>
-      replay
-    </Icon>
-  ))
+  Retry: forwardRef((props, ref) => <Replay {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
 };
