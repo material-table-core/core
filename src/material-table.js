02543a521863c0f4f6305fe882fae5ac09078e53
Subhashis Pradhan <pradhan.subhasis174@gmail.com>
2022-03-09 01:57:35 +0530

    fix: background shadow breaks for fixed columns (#503)

--- src/material-table.js
+++ src/material-table.js
@@ -1254,7 +1254,7 @@ export default class MaterialTable extends React.Component {
                             top: 0,
                             left: 0,
                             boxShadow: '2px 0px 15px rgba(125,147,178,.25)',
-                            overflowX: 'visible',
+                            overflowX: 'clip',
                             zIndex: 11
                           }}
                         >
