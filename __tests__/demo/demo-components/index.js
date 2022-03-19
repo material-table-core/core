fae55ba8f087833457ad1c244aebbeb2f6070682
Domino987 <domino987@gmail.com>
2022-03-19 15:48:59 +0100

    revert pakcage updates

--- __tests__/demo/demo-components/index.js
+++ __tests__/demo/demo-components/index.js
@@ -684,6 +684,16 @@ export function FrankensteinDemo() {
 
               resolve();
             }, 1000);
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+              resolve();
+            }, 1000);
           })
       }}
       options={{
