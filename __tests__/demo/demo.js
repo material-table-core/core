bb0b59f2c169cfb3be28c4486e5e53422e8a53eb
Domino987 <domino987@gmail.com>
2022-10-03 10:18:12 +0200

    update version to react 18

--- __tests__/demo/demo.js
+++ __tests__/demo/demo.js
@@ -21,8 +21,7 @@ import {
   StyledEngineProvider,
   createTheme
 } from '@mui/material/styles';
-import React from 'react';
-import { render } from 'react-dom';
+import React, { StrictMode } from 'react';
 
 import {
   Basic,
@@ -51,112 +50,116 @@ import {
   FixedColumnWithEdit,
   TableMultiSorting
 } from './demo-components';
+import { createRoot } from 'react-dom/client';
 import { I1353, I1941, I122 } from './demo-components/RemoteData';
 
 module.hot.accept();
 
-render(
-  <StyledEngineProvider injectFirst>
-    <ThemeProvider theme={createTheme()}>
-      <h1>DetailPanelRemounting</h1>
-      <DetailPanelRemounting />
-      {/* <h1>Switcher</h1>
-      <DataSwitcher />
+const container = document.querySelector('#app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <StrictMode>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={createTheme()}>
+        <h1>DetailPanelRemounting</h1>
+        <DetailPanelRemounting />
+        {/* <h1>Switcher</h1>
+    <DataSwitcher />
 
-    <h1>SelectionOnRowClick</h1>
-    <SelectionOnRowClick />
+  <h1>SelectionOnRowClick</h1>
+  <SelectionOnRowClick />
 
-      <h1>EventTargetErrorOnRowClick</h1>
-      <EventTargetErrorOnRowClick
-        onSelectionChange={(d) => console.log('onSelectionChange', d)}
-      />
+    <h1>EventTargetErrorOnRowClick</h1>
+    <EventTargetErrorOnRowClick
+      onSelectionChange={(d) => console.log('onSelectionChange', d)}
+    />
 
-      <h1>DetailPanelIssuesProgrammaticallyHidingWhenOpen</h1>
-      <DetailPanelIssuesProgrammaticallyHidingWhenOpen />
+    <h1>DetailPanelIssuesProgrammaticallyHidingWhenOpen</h1>
+    <DetailPanelIssuesProgrammaticallyHidingWhenOpen />
 
-      <h1>Basic</h1>
-      <Basic />
+    <h1>Basic</h1>
+    <Basic />
 
-      <h1>Basic Ref</h1>
-      <BasicRef />
+    <h1>Basic Ref</h1>
+    <BasicRef />
 
-      {/*
-      <h1>Export Data</h1>
-      <ExportData />
-      */}
-      {/*
-     <h1>Custom Export </h1>
-     <CustomExport />
-     */}
-      <h1>Bulk Edit</h1>
-      <BulkEdit />
-      <h1>Default Order Issue</h1>
-      <DefaultOrderIssue />
-      <h1>Bulk Edit With Detail Panel</h1>
-      <BulkEditWithDetailPanel />
-      <h1>Hiding Columns</h1>
-      <HidingColumns />
-      <h1>TestingNewActionHandlersProp</h1>
-      <TestingNewActionHandlersProp />
-      <h1>Editable Rows</h1>
-      <EditableRow />
-      <h1>One Detail Panel</h1>
-      <OneDetailPanel />
-      <h1>Multiple Detail Panels</h1>
-      <MultipleDetailPanels />
-      <h1>Editable</h1>
-      <EditableCells />
-      <h1>Frankenstein</h1>
-      <FrankensteinDemo />
-      <h1>Resizable Columns</h1>
-      <Resizable />
-      <h1>Persistent Groupings</h1>
-      <PersistentGroupings persistentGroupingsId="persistence-id" />
-      <h1>Persistent Groupings Same ID</h1>
-      <PersistentGroupings persistentGroupingsId="persistence-id" />
-      <h1>Persistent Groupings unshared</h1>
-      <PersistentGroupings persistentGroupingsId="some-other-id" />
-      <h1>Tree data</h1>
-      <TreeData />
-      <h1>Table with Summary Row</h1>
-      <TableWithSummary />
-      <h1>
-        Table with custom numbers of pages around current page in stepped
-        navigation
-      </h1>
-      <TableWithNumberOfPagesAround />
-      <h1>Fixed Column with Row Edits</h1>
-      <FixedColumnWithEdit />
-      <h1>Remote Data Related</h1>
-      <ol>
-        <li>
-          <h3>
-            mbrn{' '}
-            <a href="https://github.com/mbrn/material-table/issues/1353">
-              #1353
-            </a>
-          </h3>
-          <I1353 />
-        </li>
-        <li>
-          <h3>
-            mbrn{' '}
-            <a href="https://github.com/mbrn/material-table/issues/1941">
-              #1941
-            </a>
-          </h3>
-          <I1941 />
-        </li>
-        <li>
-          <h3>
-            <a href="https://github.com/material-table-core/core/issues/122">
-              #122
-            </a>
-          </h3>
-          <I122 />
-        </li>
-      </ol>
-    </ThemeProvider>
-  </StyledEngineProvider>,
-  document.querySelector('#app')
+    {/*
+    <h1>Export Data</h1>
+    <ExportData />
+    */}
+        {/*
+   <h1>Custom Export </h1>
+   <CustomExport />
+   */}
+        <h1>Bulk Edit</h1>
+        <BulkEdit />
+        <h1>Default Order Issue</h1>
+        <DefaultOrderIssue />
+        <h1>Bulk Edit With Detail Panel</h1>
+        <BulkEditWithDetailPanel />
+        <h1>Hiding Columns</h1>
+        <HidingColumns />
+        <h1>TestingNewActionHandlersProp</h1>
+        <TestingNewActionHandlersProp />
+        <h1>Editable Rows</h1>
+        <EditableRow />
+        <h1>One Detail Panel</h1>
+        <OneDetailPanel />
+        <h1>Multiple Detail Panels</h1>
+        <MultipleDetailPanels />
+        <h1>Editable</h1>
+        <EditableCells />
+        <h1>Frankenstein</h1>
+        <FrankensteinDemo />
+        <h1>Resizable Columns</h1>
+        <Resizable />
+        <h1>Persistent Groupings</h1>
+        <PersistentGroupings persistentGroupingsId="persistence-id" />
+        <h1>Persistent Groupings Same ID</h1>
+        <PersistentGroupings persistentGroupingsId="persistence-id" />
+        <h1>Persistent Groupings unshared</h1>
+        <PersistentGroupings persistentGroupingsId="some-other-id" />
+        <h1>Tree data</h1>
+        <TreeData />
+        <h1>Table with Summary Row</h1>
+        <TableWithSummary />
+        <h1>
+          Table with custom numbers of pages around current page in stepped
+          navigation
+        </h1>
+        <TableWithNumberOfPagesAround />
+        <h1>Fixed Column with Row Edits</h1>
+        <FixedColumnWithEdit />
+        <h1>Remote Data Related</h1>
+        <ol>
+          <li>
+            <h3>
+              mbrn{' '}
+              <a href="https://github.com/mbrn/material-table/issues/1353">
+                #1353
+              </a>
+            </h3>
+            <I1353 />
+          </li>
+          <li>
+            <h3>
+              mbrn{' '}
+              <a href="https://github.com/mbrn/material-table/issues/1941">
+                #1941
+              </a>
+            </h3>
+            <I1941 />
+          </li>
+          <li>
+            <h3>
+              <a href="https://github.com/material-table-core/core/issues/122">
+                #122
+              </a>
+            </h3>
+            <I122 />
+          </li>
+        </ol>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </StrictMode>
 );
