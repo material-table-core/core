998f0d8aaf1e49f450a64c0d108aca5b5d641ad9
Domino987 <domino987@gmail.com>
2022-10-22 11:29:23 +0200

    add pagination Localization test

---
+++ __tests__/localization.test.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {
+  act,
+  screen,
+  render,
+  waitForElementToBeRemoved,
+  within,
+  waitFor,
+  fireEvent
+} from '@testing-library/react';
+import MaterialTable from '../src';
+
+const lookup = { 1: 'One', 2: 'Two' };
+
+const columns = [
+  { title: 'Enum', field: 'enum', lookup },
+  { title: 'Name', field: 'id' }
+];
+
+const data = [{ id: 1, enum: 1 }];
+
+describe('Localization', () => {
+  test('Renders the pagination', () => {
+    render(
+      <MaterialTable
+        data={data}
+        columns={columns}
+        localization={{
+          pagination: {
+            labelDisplayedRows: 'Test_labelDisplayedRows',
+            labelRowsPerPage: 'Test_labelRowsPerPage',
+            labelRowsSelect: 'Test_labelRowsSelect'
+          }
+        }}
+      />
+    );
+    screen.getByText(/test_labeldisplayedrows/i);
+    screen.getByText(/test_labelrowsperpage/i);
+    screen.getByText(/5 test_labelrowsselect/i);
+  });
+});
