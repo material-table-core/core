15c507fb47c931c79e7444184c8b47acfa7a363e
Domino987 <domino987@gmail.com>
2023-07-01 11:04:14 +0200

    fix package versions

--- __tests__/post.build.test.js
+++ __tests__/post.build.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
 import MaterialTable from '../dist';
+import { render } from '@testing-library/react';
 
 import { makeData, columns } from './test.helper';
-
-configure({ adapter: new Adapter() });
 
 /**
  * Uses '../dist' for MaterialTable
@@ -15,14 +12,14 @@ describe('Render Table : Post Build', () => {
   // Render empty table
   describe('when attempting to render an empty table', () => {
     it('renders without crashing', () => {
-      shallow(<MaterialTable />);
+      render(<MaterialTable />);
     });
   });
   // Render table with random data
   describe('when attempting to render a table with data', () => {
     it('renders without crashing', () => {
       const data = makeData();
-      shallow(<MaterialTable data={data} columns={columns} />);
+      render(<MaterialTable data={data} columns={columns} />);
     });
   });
 });
