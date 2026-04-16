af517418cecab85ce9e8b456d05e4154775826bc
Domino987 <domino987@gmail.com>
2026-04-16 09:28:43 +0200

    add esbuild configuration for building and cleaning artifacts

---
+++ esbuild.config.js
@@ -0,0 +1,77 @@
+import esbuild from 'esbuild';
+import rimraf from 'rimraf';
+import path from 'path';
+import fs from 'fs';
+
+const { log, error } = console;
+
+/**
+ * OUTPUT PATH IS SET HERE!!
+ *
+ * relative to root of project
+ *
+ * !! NO TRAILING SLASH !!
+ */
+const BUILD_DIR = './dist/esm';
+
+log(`-Cleaning build artifacts from : '${BUILD_DIR}' `);
+
+rimraf(path.resolve(BUILD_DIR), async (err) => {
+  if (err) {
+    error(`err cleaning '${BUILD_DIR}' : ${error.stderr}`);
+    process.exit(1);
+  }
+
+  log('successfully cleaned build artifacts');
+
+  const options = {
+    entryPoints: getFilesRecursive('./src', '.js'),
+    minifySyntax: true,
+    minify: true,
+    bundle: false,
+    outdir: `${BUILD_DIR}`,
+    target: 'es6',
+    format: 'esm',
+    loader: {
+      '.js': 'jsx'
+    }
+  };
+
+  log('-Begin building');
+
+  try {
+    await esbuild.build(options);
+    log(
+      `\nSuccessfully built to '${BUILD_DIR}''\n[note] : this path is relative to the root of this project)'`
+    );
+  } catch (err) {
+    error(`\nERROR BUILDING : ${err}`);
+    process.exit(1);
+  } finally {
+    process.exit(0);
+  }
+});
+
+/**
+ * Helper functions
+ */
+
+function getFilesRecursive(dirPath, fileExtension = '') {
+  const paths = traverseDir(dirPath);
+  if (fileExtension === '') {
+    return paths;
+  }
+  return paths.filter((p) => p.endsWith(fileExtension));
+}
+
+function traverseDir(dir, filePaths = []) {
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = path.join(dir, file);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      traverseDir(fullPath, filePaths);
+    } else {
+      filePaths.push(fullPath);
+    }
+  });
+  return filePaths;
+}
